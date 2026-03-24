a453f83c7e4abfb1c00048ed770cd55ec8872a28
Deployer <deploy@example.com>
2026-03-24 21:52:28 +0530

    feat: replace Firebase Storage with Cloudinary for image uploads

--- src/main/java/com/whitehouse/service/FileService.java
+++ src/main/java/com/whitehouse/service/FileService.java
@@ -1,41 +1,25 @@
 package com.whitehouse.service;
 
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class FileService {
 
     @Autowired
-    private Storage storage;
-
-    @Value("${firebase.storage-bucket}")
-    private String bucketName;
+    private Cloudinary cloudinary;
 
     public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
-        
-        Bucket bucket = storage.get(bucketName);
-        if (bucket == null) {
-            throw new IOException("Bucket " + bucketName + " not found");
-        }
-
-        Blob blob = bucket.create(fileName, file.getBytes(), file.getContentType());
-        
-        // Construct the public URL for Firebase Storage
-        // Format: https://firebasestorage.googleapis.com/v0/b/[BUCKET_NAME]/o/[FILE_NAME]?alt=media
-        return String.format("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
-                bucketName, 
-                URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString()));
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
+                "folder", "texhub/products",
+                "resource_type", "image"
+        ));
+        return (String) uploadResult.get("secure_url");
     }
 }
